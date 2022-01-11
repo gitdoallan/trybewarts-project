55b98e762dd735b2f1b98b272717fab70bd28c90
gitdoallan <mailtoallan@icloud.com>
2022-01-11 16:02:54 -0300

    arrumando bug do commit anterior

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const emailInputValue = document.getElementById('email').value;
 const passwordInputValue = document.getElementById('senha').value;
 const entrarBtn = document.getElementById('login');
 function checkLogin() {
-    if (emailInputValue === 'tryber@teste.com' && passwordInputValue === '123456') {
-        alert('Olá, Tryber!');
-    } else {
-        alert('Email ou senha inválidos.');
-    }
+  if (emailInputValue === 'tryber@teste.com' && passwordInputValue === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
 }
-entrarBtn.addEventListener('click', checkLogin)
+entrarBtn.addEventListener('click', checkLogin);
