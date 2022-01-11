053a4ec53a2f05accbc3cf7962e570d99d3c4716
gitdoallan <mailtoallan@icloud.com>
2022-01-11 18:58:20 -0300

    requisito 21

--- script.js
+++ script.js
@@ -24,3 +24,31 @@ function enviarBtnState() {
   }
 }
 agreementState.addEventListener('click', enviarBtnState);
+
+//Requisito 21
+function sendForm(e) {
+  e.preventDefault();
+  const nome = document.getElementById('input-name').value;
+  const sobrenome = document.getElementById('input-lastname').value;
+  const email = document.getElementById('input-email').value;
+  const familia = document.querySelector('.family:checked').value;
+  const casa = document.getElementById('house').value;
+  const materias = document.querySelectorAll('.materias:checked');
+  const avaliacao = document.querySelector('.rate:checked').value;
+  const observacao = document.getElementById('textarea').value;
+  let materiasSelection = ''
+  for (let i=0; i<materias.length; i+=1) {
+    materiasSelection += `${materias[i].value} `
+  }
+  const theForm = document.getElementById('evaluation-form');
+  theForm.innerHTML = `
+  <p>Nome: ${nome} ${sobrenome}</p>
+  <p>Email: ${email}</p>
+  <p>Casa: ${casa}</p>
+  <p>Família: ${familia}</p>
+  <p>Matérias: ${materiasSelection}</p>
+  <p>Avaliação: ${avaliacao}</p>
+  <p>Observações: ${observacao}</p>
+  `
+}
+enviarBtn.addEventListener('click', sendForm);
